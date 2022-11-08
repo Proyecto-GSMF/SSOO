12c629027ad1ad5402a82785391a7c9b116ec2d9
mfascioli <46068681+mfascioli@users.noreply.github.com>
2022-11-08 00:59:07 -0300

    Update MySqlDump.sh

--- Tasks/MySqlDump.sh
+++ Tasks/MySqlDump.sh
@@ -6,5 +6,7 @@ else
 	mkdir ~/BackupBD
 fi
 
+umask 177
+
 export TAR_OPTIONS="--use-compress-program=pbzip2"
 mysqldump -u brumdb -a | fecha=$(date +%y-%m-%d-%T) > tar -cvzf gsmfBd$fecha.tar.bz2 ~/BackupBD/
