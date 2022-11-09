a619f701e98970a931bbc02d2f783b211006bbc9
mfascioli <46068681+mfascioli@users.noreply.github.com>
2022-11-08 21:13:50 -0300

    Update menuLogs.sh

--- Scripts/menuLogs.sh
+++ Scripts/menuLogs.sh
@@ -1,1 +1,36 @@
+#!/bin/bash
+opc=0
+function menuLogs ()
+{
+  echo "¿Que desea hacer?"
+  echo "1 - Ejecutar Ansible: playbookCommon"
+  echo "2 - Ejecutar Ansible: playbookServerApp"
+  echo "3 - Ejecutar Ansible: playbookServerBD"
+  echo "4 - Menu Logs"
+  echo "5 - Salir"
+}
 
+while [$opc -le 5]
+do
+  menu
+  read opc
+  case $opc in
+  1)
+    echo "Ejecutando Ansible: playbookCommon ..." 
+    ansible-playbook -i inventory playbookCommon.yml ;;
+  2)
+    echo "Ejecutando Ansible: playbookServerApp ..." 
+    ansible-playbook -i inventory playbookServerApp.yml ;;
+  3)
+    echo "Ejecutando Ansible: playbookServerBD ..." 
+    ansible-playbook -i inventory playbookServerBD.yml ;;
+  4)
+    echo "Abriendo script menuLogs.sh ..."
+    ./Scripts/menuLogs.sh ;;
+  5)
+    echo "Saliendo del script menuBase.sh ..."
+    break;;
+  *)
+    echo "Opcion Incorrecta";;
+  esac
+done
