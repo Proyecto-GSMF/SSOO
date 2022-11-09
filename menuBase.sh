7ea20d175ab3b9fa870aac881b6f5ba6f8bdc1c2
mfascioli <46068681+mfascioli@users.noreply.github.com>
2022-11-08 21:11:52 -0300

    Update menuBase.sh

--- menuBase.sh
+++ menuBase.sh
@@ -5,7 +5,8 @@ function menuBase ()
   echo "1 - Ejecutar Ansible: playbookCommon"
   echo "2 - Ejecutar Ansible: playbookServerApp"
   echo "3 - Ejecutar Ansible: playbookServerBD"
-  echo "4 - Logs"
+  echo "4 - Menu Logs"
+  echo "5 - Salir"
 }
 
 while [$opc -le 5]
@@ -25,12 +26,10 @@ do
   4)
     ./Scripts/menuLogs.sh ;;
   5)
-    echo "Salir"; 
+    echo "Saliendo del script menuBase.sh ..."
     break;;
   *)
     echo "Opcion Incorrecta";;
   esac
 done
-~
-~
 
